1567d9db739c7e3b776f6ab010a042cdb6ed94fd
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-08-21 15:10:27 +0200

    Minimal setup in tyche kvm to successfully init

--- arch/x86/kvm/tyche/nested.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TYCHE_X86_VMX_NESTED_H
-#define __TYCHE_X86_VMX_NESTED_H
-
-#include "tyche.h"
-
-int tyche_get_vmx_msr(struct nested_vmx_msrs *msrs, u32 msr_index, u64 *pdata);
-int tyche_set_vmx_msr(struct kvm_vcpu *vcpu, u32 msr_index, u64 data);
-
-#endif
