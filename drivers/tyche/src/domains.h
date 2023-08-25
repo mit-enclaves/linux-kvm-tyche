eccbf94faf7c60e59a8418754cc4efbdc3206b88
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-08-25 11:53:01 +0100

    Making tyche driver API callable from kvm-tyche.
    The driver so far relied on the domain_handle_t (a file descriptor) to
    reference a domain inside its functions.
    We won't have a file descriptor from the kvm-tyche backend, so I changed
    the API of the src/domains.c functions to take a direct pointer to the
    domain.

--- drivers/tyche/src/domains.h
+++ drivers/tyche/src/domains.h
@@ -17,79 +17,83 @@
 
 /// Describes an domain's memory segment in user process address space.
 typedef struct segment_t {
-  /// Start of the virtual memory segment.
-  usize vstart;
+	/// Start of the virtual memory segment.
+	usize vstart;
 
-  /// Size of the memory segment.
-  usize size;
+	/// Size of the memory segment.
+	usize size;
 
-  /// Protection flags.
-  memory_access_right_t flags;
+	/// Protection flags.
+	memory_access_right_t flags;
 
-  /// Type for the region: {Shared|Confidential}.
-  segment_type_t tpe;
+	/// Type for the region: {Shared|Confidential}.
+	segment_type_t tpe;
 
-  /// Segments are stored in a double linked list.
-  dll_elem(struct segment_t, list);
+	/// Segments are stored in a double linked list.
+	dll_elem(struct segment_t, list);
 } segment_t;
 
 /// An entry point on a core for the domain.
 typedef struct entry_t {
-  usize cr3;
-  usize rip;
-  usize rsp;
+	usize cr3;
+	usize rip;
+	usize rsp;
 } entry_t;
 
 /// All entry points for the domain.
 typedef struct entries_t {
-  /// One entry per core, total number of entries.
-  size_t size;
-  /// The entries dynamically allocated.
-  entry_t* entries;
+	/// One entry per core, total number of entries.
+	size_t size;
+	/// The entries dynamically allocated.
+	entry_t *entries;
 } entries_t;
 
 /// Describes an domain.
 typedef struct driver_domain_t {
-  /// The creator task's pid.
-  pid_t pid;
+	/// The creator task's pid.
+	pid_t pid;
 
-  /// The domain's handle within the driver.
-  struct file* handle;
-  //domain_handle_t handle;
+	/// The domain's handle within the driver.
+	domain_handle_t handle;
 
-  /// The domain's domain id.
-  domain_id_t domain_id;
+	/// The domain's domain id.
+	domain_id_t domain_id;
 
-  /// The start of the domain's physical contiguous memory region.
-  usize phys_start;
+	/// The start of the domain's physical contiguous memory region.
+	usize phys_start;
 
-  /// The start of the domain's virtual memory region in the untrusted process.
-  usize virt_start;
+	/// The start of the domain's virtual memory region in the untrusted process.
+	usize virt_start;
 
-  /// The size of the domain's contiguous memory region.
-  usize size;
+	/// The size of the domain's contiguous memory region.
+	usize size;
 
-  /// The domain's traps.
-  usize traps;
+	/// The domain's traps.
+	usize traps;
 
-  /// The domain's core map.
-  usize cores;
+	/// The domain's core map.
+	usize cores;
 
-  /// The domain's permission.
-  usize perm;
+	/// The domain's permission.
+	usize perm;
 
-  /// The domain's switch value.
-  usize switch_type;
+	/// The domain's switch value.
+	usize switch_type;
 
-  /// The domain's entry points per core.
-  entries_t entries;
+	/// The domain's entry points per core.
+	entries_t entries;
 
-  /// The segments for the domain.
-  dll_list(segment_t, segments);
+	/// The segments for the domain.
+	dll_list(segment_t, segments);
 
-  /// Domains are stored in a global list by the driver.
-  dll_elem(struct driver_domain_t, list);
+	/// Domains are stored in a global list by the driver.
+	dll_elem(struct driver_domain_t, list);
 } driver_domain_t;
+
+// ———————————————————————————————— Helpers ————————————————————————————————— //
+
+// Find a currently active domain from a file descriptor.
+driver_domain_t *find_domain(domain_handle_t handle);
 
 // ——————————————————————————————— Functions ———————————————————————————————— //
 
@@ -101,38 +105,28 @@ int driver_init_capabilities(void);
 int driver_create_domain(domain_handle_t handle);
 /// Handles an mmap call to the driver.
 /// This reserves a contiguous region and registers it until a domain claims it.
-int driver_mmap_segment(domain_handle_t domain, struct vm_area_struct* vma);
+int driver_mmap_segment(driver_domain_t *domain, struct vm_area_struct *vma);
 /// Returns the domain's physoffset.
 /// We expect the handle to be valid, and the virtaddr to exist in segments.
-int driver_get_physoffset_domain(
-    domain_handle_t handle,
-    usize* phys_offset);
+int driver_get_physoffset_domain(driver_domain_t *domain, usize *phys_offset);
 /// Sets up access rights and conf|share for the segment.
-int driver_mprotect_domain(
-    domain_handle_t handle,
-    usize vstart,
-    usize size,
-    memory_access_right_t flags,
-    segment_type_t tpe);
+int driver_mprotect_domain(driver_domain_t *domain, usize vstart, usize size,
+			   memory_access_right_t flags, segment_type_t tpe);
 /// Register the trap bitmap for the domain.
-int driver_set_traps(domain_handle_t handle, usize traps);
+int driver_set_traps(driver_domain_t *domain, usize traps);
 /// Register the core map for the domain.
-int driver_set_cores(domain_handle_t handle, usize core_map);
+int driver_set_cores(driver_domain_t *domain, usize core_map);
 /// Register the perm for the domain.
-int driver_set_perm(domain_handle_t handle, usize perm);
+int driver_set_perm(driver_domain_t *domain, usize perm);
 /// Register the switch_type for the domain.
-int driver_set_switch(domain_handle_t handle, usize sw);
+int driver_set_switch(driver_domain_t *domain, usize sw);
 /// Set the entry point on a core.
-int driver_set_entry_on_core(
-    domain_handle_t handle,
-    usize core,
-    usize cr3,
-    usize rip,
-    usize rsp);
+int driver_set_entry_on_core(driver_domain_t *domain, usize core, usize cr3,
+			     usize rip, usize rsp);
 /// Commits the domain. This is where the capability operations are done.
-int driver_commit_domain(domain_handle_t handle);
+int driver_commit_domain(driver_domain_t *domain);
 /// Implements the transition into a domain.
-int driver_switch_domain(domain_handle_t handle, void* args);
+int driver_switch_domain(driver_domain_t *domain, void *args);
 /// Delete the domain and revoke the capabilities.
-int driver_delete_domain(domain_handle_t handle);
+int driver_delete_domain(driver_domain_t *domain);
 #endif /*__SRC_DOMAINS_H__*/
