cb248d7a15d3fa46f91f93f7765fd19ee5474c73
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-09-13 13:48:16 +0100

    Started working on segments
    TODO: I think I need to change the driver to be less lazy in
    constructing the domain.
    Otherwise, we will store way too much state everywhere.

--- arch/x86/kvm/tyche/tyche.h
+++ arch/x86/kvm/tyche/tyche.h
@@ -26,8 +26,34 @@ struct kvm_tyche {
 	driver_domain_t *domain;
 };
 
+// ——————————————————————————— Segment structures ——————————————————————————— //
+typedef struct {
+	unsigned selector;
+	unsigned base;
+	unsigned limit;
+	unsigned ar_bytes;
+} kvm_tyche_segment_field_t;
+
+// ———————————————————————————— Vcpu structures ————————————————————————————— //
+
+typedef enum {
+	CS = 0,
+	DS = 1,
+	ES = 2,
+	FS = 3,
+	GS = 4,
+	SS = 5,
+	TR = 6,
+	LDTR = 7,
+	Seg_End = 8,
+} seg_idx_t;
+
 struct vcpu_tyche {
 	struct kvm_vcpu vcpu;
+	// Registers.
+	u64 regs[NR_VCPU_REGS];
+	// Segments.
+	struct kvm_segment segs[Seg_End];
 };
 
 #endif /* __KVM_X86_TYCHE_H */
