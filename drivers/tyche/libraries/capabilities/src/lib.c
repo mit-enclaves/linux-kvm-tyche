ae66ef6ad9a073cd677ef432c394e6d837fbca0e
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-09-21 10:44:44 +0200

    Adding group to set config core

--- drivers/tyche/libraries/capabilities/src/lib.c
+++ drivers/tyche/libraries/capabilities/src/lib.c
@@ -183,14 +183,14 @@ failure:
   return FAILURE;
 }
 
-int set_domain_core_configuration(domain_id_t id, usize core, usize idx, usize value)
+int set_domain_core_configuration(domain_id_t id, usize core, usize group, usize idx, usize value)
 {
   child_domain_t *child = find_child(id);
   if (child == NULL) {
     ERROR("Child not found.");
     goto failure;
   }
-  if (tyche_set_domain_core_config(child->management->local_id, core, idx, value) != SUCCESS) {
+  if (tyche_set_domain_core_config(child->management->local_id, core, group, idx, value) != SUCCESS) {
     ERROR("Unable to set core config %lld for dom %llx", idx, id);
     goto failure;
   }
